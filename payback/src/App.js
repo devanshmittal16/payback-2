578e306e5a806b2044106739f3f0cd32e4a30a22
Devansh <devansh260.be22@chitkara.edu.in>
2024-09-03 12:35:23 +0530

    initial commit

---
+++ payback/src/App.js
@@ -0,0 +1,11 @@
+import LoginForm from './Components/Loginform/LoginForm';
+
+function App() {
+  return (
+    <div>
+      <LoginForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ payback/src/Components/Loginform/LoginForm.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './LoginForm.css';
+import { FaUser, FaLock } from "react-icons/fa";
+
+const LoginForm = () => {
+    return (
+        <div className='wrapper'>
+            <form action="">
+                <h1>Login</h1>
+                <div className="input-box">
+                    <input type="email" placeholder='Username' required />
+                    <FaUser className='icon' />
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder='Password' required />
+                    <FaLock className='icon' />
+                </div>
+                <div className="forgot">
+                    <a href="#">Forgot password?</a>
+                </div>
+                <button type='submit'>Login</button>
+                <div className="sign-up">
+                    <p>Don't have an account? <a href="#">Sign up</a></p>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default LoginForm;
